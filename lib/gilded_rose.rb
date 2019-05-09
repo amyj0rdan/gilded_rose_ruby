37926f90837d8d94302bb75d1eff0e555547e1b0
Amy Jordan <amy_jordan@live.co.uk>
2019-05-09 15:59:25 +0100

    Moves some conditionals to other methods and calls on increase/decrease_quality methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,16 +10,13 @@ class GildedRose
       else
         increase_quality(item)
       end
-      update_sell_in(item)
-      if item.sell_in < 0 && !item.name.include?('Aged Brie')
-        if item.name.include?('Backstage passes')
-          item.quality = 0
-        else
+      decrease_sell_in(item)
+      if item.sell_in < 0
+        if !item.name.include?('Aged Brie')
           decrease_quality(item)
+        else
+          increase_quality(item)
         end
-      end
-      if item.sell_in < 0 && item.name.include?('Aged Brie')
-        increase_quality(item)
       end
     end
   end
@@ -28,7 +25,8 @@ class GildedRose
 
   def decrease_quality(item)
     if item.quality > 0
-      item.quality -= 1 unless item.name == 'Sulfuras, Hand of Ragnaros'
+      item.quality -= 1 unless item.name.include?('Sulfuras')
+      item.quality = 0 if item.name.include?('Backstage passes')
     end
   end
 
@@ -45,7 +43,7 @@ class GildedRose
     end
   end
 
-  def update_sell_in(item)
+  def decrease_sell_in(item)
     item.sell_in -= 1 unless item.name.include?('Sulfuras')
   end
 end
