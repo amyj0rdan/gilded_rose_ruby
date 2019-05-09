c6b331b048bd62141baca7c633f19850c5ca01ca
Amy Jordan <amy_jordan@live.co.uk>
2019-05-09 11:17:57 +0100

    Backstage passes quality increases by 3 when there are 5 days or less to sell in

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -83,6 +83,12 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 12
     end
+
+    it "increases in quality by 3 when there are 5 days or less to sell in" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 13
+    end
   end
 
 
