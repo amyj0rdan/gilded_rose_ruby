aae8378205ccc14c54d8b402523ba6ab94f32add
Amy Jordan <amy_jordan@live.co.uk>
2019-05-09 10:52:54 +0100

    Adds tests for when quality and sell_in are set to 0 on initialize

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -23,6 +23,18 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 9
     end
+
+    it "does not reduce quality below 0" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+
+    it "reduces sell_in below 0" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq -1
+    end
   end
 
 end
