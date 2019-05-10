ca2ae799bed28f224b317b8de0badce08c34652a
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 16:52:32 +0100

    Conjured #update_quality reduces quality by 2 after sell_in date passed

--- lib/conjured.rb
+++ lib/conjured.rb
@@ -3,6 +3,7 @@ require 'item'
 class Conjured < Item
   def update_quality
     @sell_in -= 1
-    @quality -= 2 if @quality > 0
+    @quality -= 2 if @sell_in >= 0 && @quality > 0
+    @quality -= 4 if @sell_in <= 0 && @quality > 0
   end
 end
--- spec/conjured_spec.rb
+++ spec/conjured_spec.rb
@@ -8,6 +8,12 @@ describe Conjured do
       expect(conjured.sell_in).to eq 9
     end
 
+    it 'reduces sell_in below 0' do
+      conjured = Conjured.new('Conjured Mana Cake', 0, 10)
+      conjured.update_quality
+      expect(conjured.sell_in).to eq(-1)
+    end
+
     it 'decreases quality by 2' do
       conjured = Conjured.new('Conjured Mana Cake', 10, 10)
       conjured.update_quality
@@ -18,6 +24,12 @@ describe Conjured do
       conjured = Conjured.new('Conjured Mana Cake', 10, 0)
       conjured.update_quality
       expect(conjured.quality).to eq 0
+    end
+
+    it 'reduces quality twice as fast after sell_in date passed' do
+      conjured = Conjured.new('Conjured Mana Cake', 0, 10)
+      conjured.update_quality
+      expect(conjured.quality).to eq(6)
     end
   end
 end
