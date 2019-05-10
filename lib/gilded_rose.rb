70b3ca2f387211da82799357cc5ed1d424b1b7fb
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 17:01:11 +0100

    Amends GildedRose (including tests) to account for different item types

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,50 +1,13 @@
 class GildedRose
 
-  INCREASE_QUALITY_ITEMS = ['Aged Brie', 'Backstage passes']
-
   def initialize(items)
     @items = items
   end
 
   def update_quality
     @items.each do |item|
-      if INCREASE_QUALITY_ITEMS.all? { |type| !item.name.include?(type) }
-        decrease_quality(item)
-      else
-        increase_quality(item)
-      end
-      decrease_sell_in(item)
-      if item.sell_in < 0
-        if !item.name.include?('Aged Brie')
-          decrease_quality(item)
-        else
-          increase_quality(item)
-        end
-      end
+      item.update_quality
     end
   end
 
-  private
-
-  def decrease_quality(item)
-    if item.quality > 0
-      item.quality -= 1 unless item.name.include?('Sulfuras') || item.name.include?('Conjured')
-      item.quality -= 2 if item.name.include?('Conjured')
-      item.quality = 0 if item.name.include?('Backstage passes')
-    end
-  end
-
-  def increase_quality(item)
-    if item.quality < 50
-      item.quality += 1
-      if item.quality < 49 && item.name.include?('Backstage passes')
-        item.quality += 1 if item.sell_in.between?(6, 10)
-        item.quality += 2 if item.sell_in.between?(0, 5)
-      end
-    end
-  end
-
-  def decrease_sell_in(item)
-    item.sell_in -= 1 unless item.name.include?('Sulfuras')
-  end
 end
