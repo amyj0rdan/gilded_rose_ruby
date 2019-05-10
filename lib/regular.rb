420f2c8c7f43eacb34ced472bd2414a57674936b
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 15:48:06 +0100

    Creates Regular item class that inherits from Item

---
+++ lib/regular.rb
@@ -0,0 +1,7 @@
+class Regular < Item
+
+  def update_quality
+    @sell_in -= 1
+  end
+  
+end
---
+++ spec/regular_spec.rb
@@ -0,0 +1,11 @@
+require 'regular'
+
+describe Regular do
+  describe "#update_quality" do
+    it "decreases sell_in by 1" do
+      regular = Regular.new('foo', 10, 10)
+      regular.update_quality
+      expect(regular.sell_in).to eq 9
+    end
+  end
+end
