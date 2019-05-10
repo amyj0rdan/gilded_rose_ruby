2dcb64158a3f92d7305348996b3895cca1c5b46e
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 16:04:52 +0100

    sell_in can go below 0

--- lib/regular.rb
+++ lib/regular.rb
@@ -2,7 +2,8 @@ class Regular < Item
 
   def update_quality
     @sell_in -= 1
-    @quality -= 1 if @sell_in > 0
+    @quality -= 1 unless @quality < 1
+    @quality -= 1 if @sell_in < 0 && @quality > 1
   end
 
 end
--- spec/regular_spec.rb
+++ spec/regular_spec.rb
@@ -8,7 +8,7 @@ describe Regular do
       expect(regular.sell_in).to eq 9
     end
 
-    it 'decreases quality of item by 1' do
+    it 'decreases quality of item by 1 when sell_in is above 0' do
       regular = Regular.new('foo', 10, 10)
       regular.update_quality
       expect(regular.quality).to eq 9
@@ -18,6 +18,18 @@ describe Regular do
       regular = Regular.new('foo', 0, 0)
       regular.update_quality
       expect(regular.quality).to eq 0
+    end
+
+    it 'reduces sell_in below 0' do
+      regular = Regular.new('foo', 0, 0)
+      regular.update_quality
+      expect(regular.sell_in).to eq(-1)
+    end
+
+    it 'decreases quality of an item by 2 when sell_in is below 0' do
+      regular = Regular.new('foo', 0, 10)
+      regular.update_quality
+      expect(regular.quality).to eq 8
     end
   end
 end
