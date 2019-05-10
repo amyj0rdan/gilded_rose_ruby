148d44bc3755f2ce5c4a0e0a32601534096163bb
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 16:48:33 +0100

    Conjured #update_quality reduces quality by 2

--- lib/conjured.rb
+++ lib/conjured.rb
@@ -3,5 +3,6 @@ require 'item'
 class Conjured < Item
   def update_quality
     @sell_in -= 1
+    @quality -= 2
   end
 end
--- spec/conjured_spec.rb
+++ spec/conjured_spec.rb
@@ -7,5 +7,11 @@ describe Conjured do
       conjured.update_quality
       expect(conjured.sell_in).to eq 9
     end
+
+    it 'decreases quality by 2' do
+      conjured = Conjured.new('Conjured Mana Cake', 10, 10)
+      conjured.update_quality
+      expect(conjured.quality).to eq 8
+    end
   end
 end
