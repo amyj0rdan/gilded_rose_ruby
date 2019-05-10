50ebff8a7bcca76014f94cae229f001258cd8243
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 15:55:42 +0100

    #update_quality does not decrease quality of an item below 0

--- lib/regular.rb
+++ lib/regular.rb
@@ -2,6 +2,7 @@ class Regular < Item
 
   def update_quality
     @sell_in -= 1
+    @quality -= 1 if @sell_in > 0
   end
-  
+
 end
--- spec/regular_spec.rb
+++ spec/regular_spec.rb
@@ -7,5 +7,17 @@ describe Regular do
       regular.update_quality
       expect(regular.sell_in).to eq 9
     end
+
+    it "decreases quality of item by 1" do
+      regular = Regular.new('foo', 10, 10)
+      regular.update_quality
+      expect(regular.quality).to eq 9
+    end
+
+    it "does not decrease quality of an item below 0" do
+      regular = Regular.new('foo', 0, 0)
+      regular.update_quality
+      expect(regular.quality).to eq 0
+    end
   end
 end
