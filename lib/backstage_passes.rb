277318233e22d6e1d57b4d128177735ac4d5f102
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 16:31:52 +0100

    BackstagePasses #update_quality reduces sell_in value

--- lib/backstage_passes.rb
+++ lib/backstage_passes.rb
@@ -2,6 +2,7 @@ require 'item'
 
 class BackstagePasses < Item
   def update_quality
+    @sell_in -= 1
     @quality += 1
   end
 end
--- spec/backstage_passes_spec.rb
+++ spec/backstage_passes_spec.rb
@@ -7,5 +7,11 @@ describe BackstagePasses do
       backstage_passes.update_quality
       expect(backstage_passes.quality).to eq 21
     end
+
+    it 'updates sell_in date' do
+      backstage_passes = BackstagePasses.new('Backstage passes to a TAFKAL80ETC concert', 20, 20)
+      backstage_passes.update_quality
+      expect(backstage_passes.sell_in).to eq 19
+    end
   end
 end
