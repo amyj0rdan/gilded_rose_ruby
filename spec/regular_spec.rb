40f971dc853004bc6a2539876d8c0111036a49d0
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 15:56:05 +0100

    Tidies single and double quote marks

--- spec/regular_spec.rb
+++ spec/regular_spec.rb
@@ -1,20 +1,20 @@
 require 'regular'
 
 describe Regular do
-  describe "#update_quality" do
-    it "decreases sell_in by 1" do
+  describe '#update_quality' do
+    it 'decreases sell_in by 1' do
       regular = Regular.new('foo', 10, 10)
       regular.update_quality
       expect(regular.sell_in).to eq 9
     end
 
-    it "decreases quality of item by 1" do
+    it 'decreases quality of item by 1' do
       regular = Regular.new('foo', 10, 10)
       regular.update_quality
       expect(regular.quality).to eq 9
     end
 
-    it "does not decrease quality of an item below 0" do
+    it 'does not decrease quality of an item below 0' do
       regular = Regular.new('foo', 0, 0)
       regular.update_quality
       expect(regular.quality).to eq 0
