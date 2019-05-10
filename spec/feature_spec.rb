f818b6324620f0cc78e734a7c4ac917275dd19b5
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 19:46:02 +0100

    Adds feature test for #update_quality - sell_in date updated

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -9,20 +9,20 @@ require 'sulfuras'
 describe "Feature test" do
   before(:each) do
     @items = [
-      Regular.new(name = '+5 Dexterity Vest', sell_in = 10, quality = 20),
-      AgedBrie.new(name = 'Aged Brie', sell_in = 2, quality = 0),
-      Regular.new(name = 'Elixir of the Mongoose', sell_in = 5, quality = 7),
-      Sulfuras.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = 0, quality = 80),
-      Sulfuras.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = -1, quality = 80),
-      BackstagePasses.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 15, quality = 20),
-      BackstagePasses.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 10, quality = 49),
-      BackstagePasses.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 5, quality = 49),
-      Conjured.new(name = 'Conjured Mana Cake', sell_in = 3, quality = 6)
+      Regular.new('+5 Dexterity Vest', 10, 20),
+      AgedBrie.new('Aged Brie', 2, 0),
+      Regular.new('Elixir of the Mongoose', 5, 7),
+      Sulfuras.new('Sulfuras, Hand of Ragnaros', 0, 80),
+      Sulfuras.new('Sulfuras, Hand of Ragnaros', -1, 80),
+      BackstagePasses.new('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+      BackstagePasses.new('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+      BackstagePasses.new('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+      Conjured.new('Conjured Mana Cake', 3, 6)
     ]
   end
 
   describe '#update_quality' do
-    it 'updates the quality of all items' do
+    it 'updates the quality of the relevant items' do
       GildedRose.new(@items).update_quality
       expect(@items[0].quality).to eq 19
       expect(@items[1].quality).to eq 1
@@ -33,6 +33,19 @@ describe "Feature test" do
       expect(@items[6].quality).to eq 50
       expect(@items[7].quality).to eq 50
       expect(@items[8].quality).to eq 4
+    end
+
+    it 'updates the sell_in date of the relevant items' do
+      GildedRose.new(@items).update_quality
+      expect(@items[0].sell_in).to eq 9
+      expect(@items[1].sell_in).to eq 1
+      expect(@items[2].sell_in).to eq 4
+      expect(@items[3].sell_in).to eq 0
+      expect(@items[4].sell_in).to eq(-1)
+      expect(@items[5].sell_in).to eq 14
+      expect(@items[6].sell_in).to eq 9
+      expect(@items[7].sell_in).to eq 4
+      expect(@items[8].sell_in).to eq 2
     end
   end
 
