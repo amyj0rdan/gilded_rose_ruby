2061e344bd3bf280966e1e33f7ec71bb0e508529
Amy Jordan <amy_jordan@live.co.uk>
2019-05-10 16:33:59 +0100

    BackstagePasses increases in quality by 2 when there are less than 10 days to sell in

--- lib/backstage_passes.rb
+++ lib/backstage_passes.rb
@@ -4,5 +4,6 @@ class BackstagePasses < Item
   def update_quality
     @sell_in -= 1
     @quality += 1
+    @quality += 1 if @sell_in < 10
   end
 end
--- spec/backstage_passes_spec.rb
+++ spec/backstage_passes_spec.rb
@@ -8,10 +8,16 @@ describe BackstagePasses do
       expect(backstage_passes.quality).to eq 21
     end
 
-    it 'updates sell_in date' do
+    it 'decreases sell_in date' do
       backstage_passes = BackstagePasses.new('Backstage passes to a TAFKAL80ETC concert', 20, 20)
       backstage_passes.update_quality
       expect(backstage_passes.sell_in).to eq 19
+    end
+
+    it 'increases in quality by 2 when there are 6 to 10 days to sell in' do
+      backstage_passes = BackstagePasses.new('Backstage passes to a TAFKAL80ETC concert', 10, 20)
+      backstage_passes.update_quality
+      expect(backstage_passes.quality).to eq 22
     end
   end
 end
